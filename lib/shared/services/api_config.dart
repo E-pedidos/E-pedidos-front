d7d0311aa1eae1d4686016456c316c8df29ca214
e-pedidos <epedidosapp2023@gmail.com>
2023-11-06 18:47:54 -0300

    chore: add function token

--- lib/shared/services/api_config.dart
+++ lib/shared/services/api_config.dart
@@ -1,6 +1,11 @@
 class ApiConfig {
   static const String baseUrl = 'http://epedidosapp.info:8000';
-  static const Map<String, String> headers = {
-    'Content-Type': 'application/json'
+  static Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Authorization': '', 
   };
+
+  static void setToken(String token) {
+    headers['Authorization'] = 'Bearer $token';
+  }
 }
