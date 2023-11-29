ff519201a716e77c3362721dfe090944891ff0b5
e-pedidos <epedidosapp2023@gmail.com>
2023-11-29 10:33:32 -0300

    chore: atualizado para enviar multpart para a api

--- lib/shared/services/api_config.dart
+++ lib/shared/services/api_config.dart
@@ -3,10 +3,17 @@ class ApiConfig {
   static Map<String, String> headers = {
     'Content-Type': 'application/json;charset=UTF-8',
     'Charset': 'utf-8',
-    'Authorization': '', 
+    'Authorization': '',
+  };
+
+  static Map<String, String> multipartHeaders = {
+    'Content-Type': 'multipart/form-data',
+    'Charset': 'utf-8',
+    'Authorization': '',
   };
 
   static void setToken(String token) {
     headers['Authorization'] = 'Bearer $token';
+    multipartHeaders['Authorization'] = 'Bearer $token';
   }
 }
