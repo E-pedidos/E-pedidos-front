13267c9b889f50b831a061ff5b58e6b922a2d099
e-pedidos <epedidosapp2023@gmail.com>
2024-01-05 15:33:01 -0300

    chore: corrigido estruturação

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -55,88 +55,90 @@ class _HomePageState extends State<HomePage> {
         BlocProvider<OrderBloc>.value(value: _orderBloc),
       ],
       child: BlocBuilder<FilialBloc, FilialState>(builder: (context, state) {
+        if (state is FilialLoadingState) {
+          return const Center(
+              child: CircularProgressIndicator(
+            color: Colors.orange,
+          ));
+        }
         if (state is FilialLoadedState) {
           final filials = state.filiais;
           getOrder(filials);
           return BlocBuilder<OrderBloc, OrderState>(
               builder: (context, stateOrder) {
             final orders = stateOrder.orders;
-            if (stateOrder is OrderLoadingState) {
-              return const Expanded(
-                child: Center(
-                    child: CircularProgressIndicator(
-                  color: Colors.orange,
-                )),
-              );
-            }
-            return CustomLayout(
-              child: Scaffold(
-                backgroundColor: Colors.white,
-                body: Padding(
-                  padding: const EdgeInsets.fromLTRB(31, 25, 31, 100),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      filials.isEmpty
-                          ? const Center(
-                              child: Text(
-                              'Cadastre uma filial',
-                              style: TextStyle(fontWeight: FontWeight.w700),
-                            ))
-                          : SizedBox(
-                              width: double.infinity,
-                              child: DropdownButton<String>(
-                                  value: dropdownValue,
-                                  icon: const Icon(Icons.arrow_drop_down),
-                                  style: const TextStyle(
-                                      color: Colors.black, fontSize: 18),
-                                  underline: Container(
-                                    height: 2,
-                                    color:
-                                        const Color.fromRGBO(54, 148, 178, 1),
-                                  ),
-                                  onChanged: (String? newValue) async {
-                                    SharedPreferences sharedPreferences =
-                                        await SharedPreferences.getInstance();
+            if (stateOrder is OrderLoadedState) {
+              return CustomLayout(
+                child: Scaffold(
+                  backgroundColor: Colors.white,
+                  body: Padding(
+                    padding: const EdgeInsets.fromLTRB(31, 25, 31, 100),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        filials.isEmpty
+                            ? const Center(
+                                child: Text(
+                                'Cadastre uma filial',
+                                style: TextStyle(fontWeight: FontWeight.w700),
+                              ))
+                            : SizedBox(
+                                width: double.infinity,
+                                child: DropdownButton<String>(
+                                    value: dropdownValue,
+                                    icon: const Icon(Icons.arrow_drop_down),
+                                    style: const TextStyle(
+                                        color: Colors.black, fontSize: 18),
+                                    underline: Container(
+                                      height: 2,
+                                      color:
+                                          const Color.fromRGBO(54, 148, 178, 1),
+                                    ),
+                                    onChanged: (String? newValue) async {
+                                      SharedPreferences sharedPreferences =
+                                          await SharedPreferences.getInstance();
 
-                                    setState(() {
-                                      dropdownValue = newValue!;
-                                    });
+                                      setState(() {
+                                        dropdownValue = newValue!;
+                                      });
 
-                                    await sharedPreferences.setString(
-                                        'idFilial', dropdownValue!);
-                                  },
-                                  items: filials.map((FilialModel filial) {
-                                    return DropdownMenuItem<String>(
-                                      value: filial.id!,
-                                      child: Text(filial.name!.toString()),
-                                    );
-                                  }).toList())),
-                      const Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 21),
-                          child: Text(
-                            'Mesas',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w500),
-                          )),
-                      ConatainerTable(list: orders),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      const Text(
-                        'Pedidos',
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.w500),
-                      ),
-                      const SizedBox(
-                        height: 13,
-                      ),
-                      ConatainerList(list: orders)
-                    ],
+                                      await sharedPreferences.setString(
+                                          'idFilial', dropdownValue!);
+                                    },
+                                    items: filials.map((FilialModel filial) {
+                                      return DropdownMenuItem<String>(
+                                        value: filial.id!,
+                                        child: Text(filial.name!.toString()),
+                                      );
+                                    }).toList())),
+                        const Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 0, 0, 21),
+                            child: Text(
+                              'Mesas',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w500),
+                            )),
+                        ConatainerTable(list: orders),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        const Text(
+                          'Pedidos',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.w500),
+                        ),
+                        const SizedBox(
+                          height: 13,
+                        ),
+                        ConatainerList(list: orders)
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
+              );
+            }
+            return const SizedBox();
           });
         }
         return const SizedBox();
