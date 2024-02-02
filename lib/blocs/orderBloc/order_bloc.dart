50ae9ae1ce14854c52fd9d092da5d939a0145267
E-pedidos <epedidosapp2023@gmail.com>
2024-02-02 17:34:26 -0300

    gti: fix alteração socket

--- lib/blocs/orderBloc/order_bloc.dart
+++ lib/blocs/orderBloc/order_bloc.dart
@@ -3,13 +3,44 @@ import 'package:e_pedidos_front/blocs/orderBloc/order_event.dart';
 import 'package:e_pedidos_front/blocs/orderBloc/order_state.dart';
 import 'package:e_pedidos_front/models/order_model.dart';
 import 'package:e_pedidos_front/repositorys/order_repository.dart';
+import 'package:e_pedidos_front/shared/utils/shared_preferences_utils.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
 
 class OrderBloc extends Bloc<OrderEvent, OrderState>{
-   final orderRepository = OrderRepository();
+  final orderRepository = OrderRepository();
+  SharedPreferencesUtils presf = SharedPreferencesUtils();
+  late final Socket _socket;
 
   OrderBloc(): super(OrderInitialState()){
+    _socket = io('ws://epedidosapp.info:8000/',
+       <String, dynamic>{
+      'transports': ['websocket'],
+      'upgrade': false
+    });
+
+    _socket.connect();
+
+    _socket.onConnect((data) { 
+      print(data); 
+    });
+
+    _socket.onConnectError((err) => print('Erro de conexão: $err'));
+    _socket.onError((err) => print('Erro: $err'));
+    /* _socket.emit('enter-filial',(data) {
+      print(data);
+    }); */
+
+    /* _socket.on('new-order-added', (data) {
+      // Adicione lógica para processar o novo pedido, se necessário
+      print(data);
+
+      // Após receber um novo pedido, emita o evento GetOrders para atualizar a lista
+      add(GetOrders());
+    }); */
     on(_mapEventToState);
   }
+
 
   void _mapEventToState(OrderEvent event, Emitter emit) async{
     List<OrderModel> orders = [];
@@ -23,9 +54,15 @@ class OrderBloc extends Bloc<OrderEvent, OrderState>{
         orders = res;
       } else{
         orders = [];
-      }
+      } 
     }
 
     emit(OrderLoadedState(orders: orders));
+  }
+
+  @override
+  Future<void> close() {
+    _socket.disconnect();
+    return super.close();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 import 'package:e_pedidos_front/my_app.dart';
 import 'package:flutter/material.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
-  runApp(const MyApp());
+  HttpOverrides.global = MyHttpOverrides();
+  runApp(MyApp());
 }
